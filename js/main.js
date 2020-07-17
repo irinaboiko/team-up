85941441a7acdb9880557b8beea6ed887ac7c639
Irina Boiko <irinaboiko95@gmail.com>
2020-07-17 13:07:40 +0300

    send form

--- js/main.js
+++ js/main.js
@@ -32,8 +32,8 @@ menuMobBox.addEventListener('click', event => {
 /*PROMO 100VH*/
 
 //window.addEventListener('resize', () => {
-	const vh = window.innerHeight * 0.01;
-	document.documentElement.style.setProperty('--vh', `${vh}px`);
+const vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
 //});
 
 /*slider adv*/
@@ -213,3 +213,85 @@ if(header) {
     }
   }
 } */
+
+/*PROMO FORM*/
+
+/* $(document).on('submit', '#feedback-form', function(e) {
+	e.preventDefault();
+    var fio = $('input[name=fio]'),
+		fio_text = fio.val(),
+        phone = $('input[name=phone]'),
+        phone_text = phone.val()
+    $.ajax({
+        url: "https://formspree.io/irinaboiko95@gmail.com",
+        method: "POST",
+        data: {
+            fio: fio_text,
+            phone: phone_text
+        },
+        dataType: "json"
+        //beforeSend: function( xhr ) {
+        //spinner.show();
+      //}
+    })/* .done(function(){
+	$('#contacts-success-wrapper').show();
+}).fail(function() {
+	$('#contacts-error-wrapper').show();
+}).always(function() {
+	spinner.hide();
+	$('#contacts-wrapper').remove();
+}); */
+//});
+
+/* $.ajax({
+	url: "https://formspree.io/feedback-form",
+	method: "POST",
+	data: { message: "hello!" },
+	dataType: "json"
+}); */
+
+window.addEventListener('DOMContentLoaded', function () {
+
+	// get the form elements defined in your form HTML above
+
+	var form = document.getElementById('form-promo-main');
+	var button = document.getElementById('feedback-form__button');
+	var status = document.getElementById('form-promo-main-status');
+
+	// Success and Error functions for after the form is submitted
+
+	function success() {
+		form.reset();
+		button.style = "display: none ";
+		status.innerHTML = "Thanks!";
+	}
+
+	function error() {
+		status.innerHTML = "Oops! There was a problem.";
+	}
+
+	// handle the form submission event
+
+	form.addEventListener("submit", function (ev) {
+		ev.preventDefault();
+		var data = new FormData(form);
+		ajax(form.method, form.action, data, success, error);
+	});
+});
+
+// helper function for sending an AJAX request
+
+function ajax(method, url, data, success, error) {
+	var xhr = new XMLHttpRequest();
+	xhr.open(method, url);
+	xhr.setRequestHeader("Accept", "application/json");
+	xhr.onreadystatechange = function () {
+		if (xhr.readyState !== XMLHttpRequest.DONE) return;
+		if (xhr.status === 200) {
+			success(xhr.response, xhr.responseType);
+		} else {
+			error(xhr.status, xhr.response, xhr.responseType);
+		}
+	};
+	xhr.send(data);
+}
